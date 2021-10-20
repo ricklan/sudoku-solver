cdf85d6b37b09c2a5746fd0f3f0dfe8c84c1dc70
Ying Zhou <zhouying@live.ca>
2021-10-20 18:33:11 -0400

    Add event and function to board input to update sudoku board cell array

--- sudoku-solver/src/App.js
+++ sudoku-solver/src/App.js
@@ -2,42 +2,61 @@ import "./App.css";
 
 function SudokuBoard() {
   let board = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
   ];
+
+  function processBoard(e) {
+    e.preventDefault();
+    console.log("processing board");
+  }
+
+  function updateCell(e) {
+    if (e.key >= "1" && e.key <= "9") {
+      board[parseInt(e.target.id.substring(0, 1))][
+        parseInt(e.target.id.substring(2, 3))
+      ] = e.key;
+      e.target.value = e.key;
+      console.log(`cell ${e.target.id} input value ${e.key}`);
+      console.log(board);
+    }
+  }
 
   return (
     <>
       <form>
         <div id="sudoku-board">
-          <label for="board">Enter values for the sudoku board</label>
+          <label htmlFor="board">Enter values for the sudoku board</label>
           {board.map((row, x) => {
             return (
-              <>
-                <br />
+              <div key={`row ${x}`}>
+                <br key={row} />
                 {row.map((cell, y) => {
                   return (
                     <input
                       type="text"
-                      maxlength="1"
-                      pattern="[0-9]"
+                      maxLength="1"
+                      pattern="[1-9]"
                       id={`${x}-${y}`}
+                      key={`${x}-${y}`}
+                      defaultValue={board[x][y]}
+                      onKeyPress={(e) => updateCell(e)}
                     />
                   );
                 })}
-              </>
+              </div>
             );
           })}
         </div>
         <div id="board-menu">
-          <button>Submit</button>
+          <button onClick={(e) => processBoard(e)}>Submit</button>
           <button>Clear</button>
         </div>
       </form>
