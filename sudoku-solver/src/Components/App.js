09b45dd214308b49000a3d5a02d1850ba34da40a
Ying Zhou <zhouying@live.ca>
2021-10-28 21:12:31 -0400

    Add controls component

--- sudoku-solver/src/Components/App.js
+++ sudoku-solver/src/Components/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { SudokuBoard } from "./SudokuBoard";
+import { Controls } from "./Controls";
 
 function App() {
   return (
     <div className="App">
       <SudokuBoard />
+      <Controls />
     </div>
   );
 }
---
+++ sudoku-solver/src/Components/Controls.js
@@ -0,0 +1,12 @@
+export function Controls() {
+  /*todo:add images*/
+  return (
+    <>
+      <h2>Controls</h2>
+      <p>Navigation</p>
+      <p>Input</p>
+      <p>1-9</p>
+      <p>0, Delete</p>
+    </>
+  );
+}
