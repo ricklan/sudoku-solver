778a582f23952c6588ee6d14899f70dd081a3df9
Ying Zhou <zhouying@live.ca>
2021-10-29 15:36:31 -0400

    Create Header component

--- sudoku-solver/src/Components/App.js
+++ sudoku-solver/src/Components/App.js
@@ -1,10 +1,12 @@
 import "../css/App.css";
 import { SudokuBoard } from "./SudokuBoard";
 import { BoardUse } from "./BoardUse";
+import { Header } from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SudokuBoard />
       <BoardUse />
     </div>
---
+++ sudoku-solver/src/Components/Header.js
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <>
+      <header>
+        <h1>Sudoku Solver</h1>
+      </header>
+    </>
+  );
+}
